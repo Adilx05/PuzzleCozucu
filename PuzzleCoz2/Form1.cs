4ffd4d6697573701784825236a7df3bce8234f65
Adilx05 <adilx05@gmail.com>
2017-04-03 02:01:53 +0300

    İlk
    Oluşturuldu

---
+++ PuzzleCoz2/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using Puzzle;
+
+namespace PuzzleCoz2
+{
+    public partial class Form1 : Form
+    {
+        private MantikPuzzle _m;
+        private Mantiksal _mMantiksal;
+        private LinearKaristir<int> _mKaristir;
+        private WindowsFormsSynchronizationContext mSyncContext;
+        Dictionary<int, Button> _mButtons;
+        private int[] _mInitialState;
+        private bool _mBusy;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mSyncContext = SynchronizationContext.Current as WindowsFormsSynchronizationContext;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            _mInitialState = new int[] { 8, 7, 2, 4, 6, 3, 1, -1, 5 };
+
+            _mKaristir = new LinearKaristir<int>();
+            _m = new MantikPuzzle();
+            _mMantiksal = Mantiksal.SiracilOyun;
+            _m.KonumDegismesi += KonumDegismesi;
+            _m.OnCozum += OnCozum;
+
+            // Set display nodes
+            _mButtons = new Dictionary<int, Button>();
+            _mButtons[0] = button1;
+            _mButtons[1] = button2;
+            _mButtons[2] = button3;
+            _mButtons[3] = button4;
+            _mButtons[4] = button5;
+            _mButtons[5] = button6;
+            _mButtons[6] = button7;
+            _mButtons[7] = button8;
+            _mButtons[8] = button9;
+
+            // Display state
+            DisplayState(_mInitialState, false);
+
+            statusLabel.Text = "Çözek Bakam";
+            progressBar.Style = ProgressBarStyle.Marquee;
+            progressBar.Visible = false;
+            merkezuzMenu.Checked = true;
+        }
+
+        private void DegerDegistir(int x, int y)
+        {
+            int temp = _mInitialState[x];
+            _mInitialState[x] = _mInitialState[y];
+            _mInitialState[y] = temp;
+        }
+
+        private void KonumDegismesi(int[] state, bool isFinal)
+        {
+            mSyncContext.Post(item => DisplayState(state, isFinal), null);
+            Thread.Sleep(1500);
+        }
+
+        private void OnCozum(int steps, int time, int statesExamined)
+        {
+            Action action = () =>
+            {
+                progressBar.Visible = false;
+                this.Cursor = Cursors.Default;
+
+                if (steps > -1)
+                {
+                    statusLabel.Text = "Adım: " + steps.ToString("n0") + ", Zaman: " + (time / 1000.0).ToString("n2") + ", Varyasyon: " + statesExamined.ToString("n0");
+                    MessageBox.Show(this, "Çözüm Bulundu !", "Bilgi !");
+                }
+                else
+                {
+                    statusLabel.Text = "Adım: Yok, Zaman: " + (time / 1000.0).ToString("n3") + "Saniye, Varyasyon: " + statesExamined.ToString("n0");
+                    MessageBox.Show(this, "Çözüm Bulunamadı", "Bilgi !");
+                }
+            };
+
+            mSyncContext.Send(item => action.Invoke(), null);
+        }
+
+        private void DisplayState(int[] nodes, bool isFinal)
+        {
+            if (nodes != null)
+            {
+                this.gamePanel.SuspendLayout();
+
+                for (int i = 0; i < nodes.Length; i++)
+                {
+                    if (nodes[i] > 0)
+                    {
+                        _mButtons[i].Text = nodes[i].ToString();
+                    }
+                    else
+                    {
+                        _mButtons[i].Text = null;
+                    }
+                }
+
+                this.gamePanel.ResumeLayout();
+            }
+
+            if (isFinal)
+            {
+                _mBusy = false;
+                buttonShuffle.Enabled = true;
+                buttonStart.Enabled = true;
+            }
+        }
+
+        private void CozmeyeBasla()
+        {
+            _m.Coz(_mInitialState, _mMantiksal);
+
+            progressBar.Visible = true;
+            this.Cursor = Cursors.WaitCursor;
+            statusLabel.Text = "Çözüm Bulunuyor...";
+            _mBusy = true;
+        }
+
+        private bool IslemIzni()
+        {
+            return !_mBusy;
+        }
+
+        private void Karistir_Button_Click(object sender, EventArgs e)
+        {
+            if (IslemIzni())
+            {
+                _mKaristir.Shuffle(_mInitialState);
+                DisplayState(_mInitialState, false);
+            }
+        }
+
+        private void Button_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (IslemIzni())
+            {
+                Button button = sender as Button;
+
+                if (button != null && button.Tag != null)
+                {
+                    int deger;
+                    Button tileButton;
+
+                    if (int.TryParse(button.Tag.ToString(), out deger) && _mButtons.TryGetValue(deger, out tileButton) && button == tileButton)
+                    {
+                        button.DoDragDrop(button.Tag, DragDropEffects.Copy | DragDropEffects.Move);
+                    }
+                }
+            }
+        }
+
+        private void Button_DragEnter(object sender, DragEventArgs e)
+        {
+            if (IslemIzni())
+            {
+                if (e.Data.GetDataPresent(DataFormats.Text))
+                {
+                    e.Effect = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+            }
+        }
+
+        private void Button_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (IslemIzni())
+            {
+                Button button = sender as Button;
+                if (button != null && button.Tag != null)
+                {
+                    int dropValue;
+                    Button buttonToDrop;
+
+                    if (int.TryParse(button.Tag.ToString(), out dropValue) && _mButtons.TryGetValue(dropValue, out buttonToDrop) && button == buttonToDrop)
+                    {
+                        int dragValue;
+
+                        if (int.TryParse(e.Data.GetData(DataFormats.Text).ToString(), out dragValue) && dropValue != dragValue)
+                        {
+                            DegerDegistir(dragValue, dropValue);
+                            DisplayState(_mInitialState, false);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (IslemIzni())
+            {
+                CozmeyeBasla();
+            }
+        }
+
+        private void Karistir_Menu_Click(object sender, EventArgs e)
+        {
+            if (IslemIzni())
+            {
+                _mKaristir.Shuffle(_mInitialState);
+                // Display state
+                DisplayState(_mInitialState, false);
+            }
+        }
+
+        private void Coz_Menu_Click(object sender, EventArgs e)
+        {
+            if (IslemIzni())
+            {
+                CozmeyeBasla();
+            }
+        }
+
+        private void Cikis_Menu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Merkez_Menu_Click(object sender, EventArgs e)
+        {
+            if (IslemIzni())
+            {
+                _mMantiksal = Mantiksal.SiracilOyun;
+                merkezuzMenu.Checked = true;
+                boslukitemMenu.Checked = false;
+            }
+        }
+
+        private void Bosluk_Menu_Click(object sender, EventArgs e)
+        {
+            if (IslemIzni())
+            {
+                _mMantiksal = Mantiksal.BoslugaOyna;
+                boslukitemMenu.Checked = true;
+                merkezuzMenu.Checked = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void hakkındaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Adil Tarafından Patatesli Poğaça Takımı İçin Hazırlandı :)", "Hakkında");
+        }
+    }
+
+}
---
+++ PuzzleCoz2/Strategy.cs
@@ -0,0 +1,659 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    internal enum Mantiksal
+    {
+        BoslugaOyna,
+        SiracilOyun
+    }
+
+    internal enum Yonler
+    {
+        Sol,
+        Sag,
+        Yukari,
+        Assagi,
+    }
+
+    internal sealed class State : IComparable
+    {
+        private int[] mNodes;
+        private int mSpaceIndex;
+        private string mStateCode;
+        private int mCostf;
+        private int mCosth;
+        private int mCostg;
+        private Mantiksal _mMantiksal;
+        private State mParent;
+
+        internal State(State parent, int[] nodes, Mantiksal mantiksal)
+        {
+            mNodes = nodes;
+            mParent = parent;
+            _mMantiksal = mantiksal;
+            Hesapla();
+            mStateCode = StateHesap();
+        }
+
+        private State(State parent, int[] nodes)
+        {
+            mNodes = nodes;
+            mParent = parent;
+            _mMantiksal = parent._mMantiksal;
+            Hesapla();
+            mStateCode = StateHesap();
+        }
+
+        public override bool Equals(object obj)
+        {
+            State that = obj as State;
+
+            return that != null && this.mStateCode.Equals(that.mStateCode);
+        }
+
+        public override int GetHashCode()
+        {
+            return mStateCode.GetHashCode();
+        }
+
+        public int CompareTo(object obj)
+        {
+            State that = obj as State;
+
+            if (that != null)
+            {
+                return (this.mCostf).CompareTo(that.mCostf);
+            }
+
+            return 0;
+        }
+
+        public bool IsCostlierThan(State thatState)
+        {
+            return this.mCostg > thatState.mCostg;
+        }
+
+        public String GetStateCode()
+        {
+            return mStateCode;
+        }
+
+        private void Hesapla()
+        {
+            if (mParent == null)
+            {
+                mCostg = 0;
+            }
+            else
+            {
+                mCostg = mParent.mCostg + 1;
+            }
+
+            mCosth = MantiksalDeger();
+
+            mCostf = mCosth + mCostg;
+        }
+
+        private int MantiksalDeger()
+        {
+            if (_mMantiksal == Mantiksal.SiracilOyun)
+            {
+                return MerkezMesafeDegeri();
+            }
+            else
+            {
+                return GetMisplacedTilesCost();
+            }
+        }
+
+        private int GetMisplacedTilesCost()
+        {
+            int heuristicCost = 0;
+
+            for (int i = 0; i < mNodes.Length; i++)
+            {
+                int value = mNodes[i] - 1;
+
+                // Boşluk değeri -1
+                if (value == -2)
+                {
+                    value = mNodes.Length - 1;
+                    mSpaceIndex = i;
+                }
+
+                if (value != i)
+                {
+                    heuristicCost++;
+                }
+            }
+
+            return heuristicCost;
+        }
+
+        private int MerkezMesafeDegeri()
+        {
+            int mantiksalDeger = 0;
+            int gridX = (int)Math.Sqrt(mNodes.Length);
+            int idealX;
+            int idealY;
+            int guncelX;
+            int guncelY;
+            int deger;
+
+            for (int i = 0; i < mNodes.Length; i++)
+            {
+                deger = mNodes[i] - 1;
+                if (deger == -2)
+                {
+                    deger = mNodes.Length - 1;
+                    mSpaceIndex = i;
+                }
+
+                if (deger != i)
+                {
+                    idealX = deger % gridX;
+                    idealY = deger / gridX;
+
+                    guncelX = i % gridX;
+                    guncelY = i / gridX;
+
+                    mantiksalDeger += (Math.Abs(idealY - guncelY) + Math.Abs(idealX - guncelX));
+                }
+            }
+
+            return mantiksalDeger;
+        }
+
+        private String StateHesap()
+        {
+            StringBuilder code = new StringBuilder();
+
+            for (int i = 0; i < mNodes.Length; i++)
+            {
+                code.Append(mNodes[i] + "*");
+            }
+
+            return code.ToString().Trim(new char[] { '*' });
+        }
+
+        public int[] GetState()
+        {
+            int[] state = new int[mNodes.Length];
+            Array.Copy(mNodes, state, mNodes.Length);
+
+            return state;
+        }
+
+        public bool IsFinalState() // Hepsi Doğruysa buraya akıyoz
+        {
+            return mCosth == 0;
+        }
+
+        public State GetParent()
+        {
+            return mParent;
+        }
+
+        public List<State> SiradakiYerler(ref List<State> siradakiYerler)
+        {
+            siradakiYerler.Clear();
+            State yer;
+
+            foreach (Yonler yon in Enum.GetValues(typeof(Yonler)))
+            {
+                yer = SiradakiYeriBul(yon);
+
+                if (yer != null)
+                {
+                    siradakiYerler.Add(yer);
+                }
+            }
+
+            return siradakiYerler;
+        }
+
+        private State SiradakiYeriBul(Yonler yonler)
+        {
+            int pozisyon;
+
+            if (HareketKon(yonler, out pozisyon))
+            {
+                int[] nodes = new int[mNodes.Length];
+                Array.Copy(mNodes, nodes, mNodes.Length);
+
+                Degistir(nodes, mSpaceIndex, pozisyon);
+
+                return new State(this, nodes);
+            }
+
+            return null;
+        }
+
+        private void Degistir(int[] nodes, int i, int j)
+        {
+            int t = nodes[i];
+            nodes[i] = nodes[j];
+            nodes[j] = t;
+        }
+
+        private bool HareketKon(Yonler yonler, out int yeniPozisyon)
+        {
+            int yeniX = -1;
+            int yeniY = -1;
+            int gridX = (int)Math.Sqrt(mNodes.Length);
+            int guncelX = mSpaceIndex % gridX;
+            int guncelY = mSpaceIndex / gridX;
+            yeniPozisyon = -1;
+
+            switch (yonler)
+            {
+                case Yonler.Yukari:
+                    {
+                        if (guncelY != 0)
+                        {
+                            yeniX = guncelX;
+                            yeniY = guncelY - 1;
+                        }
+                    }
+                    break;
+
+                case Yonler.Assagi:
+                    {
+                        if (guncelY < (gridX - 1))
+                        {
+                            yeniX = guncelX;
+                            yeniY = guncelY + 1;
+                        }
+                    }
+                    break;
+
+                case Yonler.Sol:
+                    {
+                        if (guncelX != 0)
+                        {
+                            yeniX = guncelX - 1;
+                            yeniY = guncelY;
+                        }
+                    }
+                    break;
+
+                case Yonler.Sag:
+                    {
+                        if (guncelX < (gridX - 1))
+                        {
+                            yeniX = guncelX + 1;
+                            yeniY = guncelY;
+                        }
+                    }
+                    break;
+            }
+
+            if (yeniX != -1 && yeniY != -1)
+            {
+                yeniPozisyon = yeniY * gridX + yeniX;
+            }
+
+            return yeniPozisyon != -1;
+        }
+
+        public override string ToString()
+        {
+            return "State:" + mStateCode + ", g:" + mCostg + ", h:" + mCosth + ", f:" + mCostf;
+        }
+    }
+
+    internal delegate void StateChanged(int[] currentState, bool isFinal);
+    internal delegate void Cozum(int steps, int time, int stateExamined);
+
+    internal sealed class MantikPuzzle
+    {
+        #region Fields
+
+        private Stopwatch mStopWatch;
+        internal event StateChanged KonumDegismesi;
+        internal event Cozum OnCozum;
+
+        #endregion Fields
+
+        #region Methods
+
+        internal MantikPuzzle()
+        {
+            mStopWatch = new Stopwatch();
+        }
+
+        internal void Coz(int[] nodes, Mantiksal mantiksal)
+        {
+            ThreadPool.QueueUserWorkItem(item => Start(nodes, mantiksal));
+        }
+
+        private void Start(int[] nodes, Mantiksal mantiksal)
+        {
+            int acikYer;
+            int yerSayisi = -1;
+            State guncelYer = null;
+            List<State> nextYer = new List<State>();
+            HashSet<String> serbestYer = new HashSet<string>();
+            MinPriorityQueue<State> serbestYerSira = new MinPriorityQueue<State>(nodes.Length * 3);
+            Dictionary<String, State> kapaliYer = new Dictionary<string, State>(nodes.Length * 3);
+
+            State state = new State(null, nodes, mantiksal);
+            serbestYerSira.Enqueue(state);
+            serbestYer.Add(state.GetStateCode());
+
+            ZamaniBaslat();
+
+            while (!serbestYerSira.IsEmpty())
+            {
+                guncelYer = serbestYerSira.Dequeue();
+                serbestYer.Remove(guncelYer.GetStateCode());
+
+                yerSayisi++;
+
+                if (guncelYer.IsFinalState())
+                {
+                    ZamaniDurdur(yerSayisi);
+                    break;
+                }
+
+                guncelYer.SiradakiYerler(ref nextYer);
+
+                if (nextYer.Count > 0)
+                {
+                    State kapali;
+                    State acik;
+                    State siradaki;
+
+                    for (int i = 0; i < nextYer.Count; i++)
+                    {
+                        kapali = null;
+                        acik = null;
+                        siradaki = nextYer[i];
+
+                        if (serbestYer.Contains(siradaki.GetStateCode()))
+                        {
+                            acik = serbestYerSira.Find(siradaki, out acikYer);
+
+                            if (acik.IsCostlierThan(siradaki))
+                            {
+                                serbestYerSira.Kaldir(acikYer);
+                                serbestYerSira.Enqueue(siradaki);
+                            }
+                        }
+                        else
+                        {
+                            String stateCode = siradaki.GetStateCode();
+
+                            if (kapaliYer.TryGetValue(stateCode, out kapali))
+                            {
+                                if (kapali.IsCostlierThan(siradaki))
+                                {
+                                    kapaliYer.Remove(stateCode);
+                                    kapaliYer[stateCode] = siradaki;
+                                }
+                            }
+                        }
+
+                        if (acik == null && kapali == null)
+                        {
+                            serbestYerSira.Enqueue(siradaki);
+                            serbestYer.Add(siradaki.GetStateCode());
+                        }
+                    }
+
+                    kapaliYer[guncelYer.GetStateCode()] = guncelYer;
+                }
+            }
+
+            if (guncelYer != null && !guncelYer.IsFinalState()) //Çözüm Bulunamadı
+            {
+                guncelYer = null;
+            }
+
+            Cozulunce(guncelYer, yerSayisi);
+            SonAsama(guncelYer);
+        }
+
+        private void ZamaniBaslat()
+        {
+            mStopWatch.Reset();
+            mStopWatch.Start();
+        }
+
+        private void ZamaniDurdur(int stateCount)
+        {
+            mStopWatch.Stop();
+        }
+
+        private void SonAsama(State state)
+        {
+            if (state != null)
+            {
+                Stack<State> path = new Stack<State>();
+
+                while (state != null)
+                {
+                    path.Push(state);
+                    state = state.GetParent();
+                }
+
+                while (path.Count > 0)
+                {
+                    KonumDegismesi(path.Pop().GetState(), path.Count == 0);
+                }
+            }
+            else
+            {
+                KonumDegismesi(null, true);
+            }
+        }
+
+        private void Cozulunce(State state, int states)
+        {
+            int adim = -1;
+
+            while (state != null)
+            {
+                state = state.GetParent();
+                adim++;
+            }
+
+            if (OnCozum != null)
+            {
+                OnCozum(adim, (int)mStopWatch.ElapsedMilliseconds, states);
+            }
+        }
+
+        #endregion Methods
+    }
+
+    internal sealed class MinPriorityQueue<T> where T : IComparable
+    {
+        #region Fields
+
+        private T[] mArray;
+        private int mCount;
+
+        #endregion Fields
+
+        #region Methods
+
+        internal MinPriorityQueue(int capacity)
+        {
+            mArray = new T[capacity + 1];
+            mCount = 0;
+        }
+
+        private void Expand(int capacity)
+        {
+            T[] temp = new T[capacity + 1];
+            int i = 0;
+            while (++i <= mCount)
+            {
+                temp[i] = mArray[i];
+                mArray[i] = default(T);
+            }
+
+            mArray = temp;
+        }
+
+        private bool Less(int i, int j)
+        {
+            return mArray[i].CompareTo(mArray[j]) < 0;
+        }
+
+        private void Degis(int i, int j)
+        {
+            T temp = mArray[j];
+            mArray[j] = mArray[i];
+            mArray[i] = temp;
+        }
+
+        private void Sink(int index)
+        {
+            int k;
+            while (index * 2 <= mCount)
+            {
+                k = index * 2;
+
+                if (k + 1 <= mCount && Less(k + 1, k))
+                {
+                    k = k + 1;
+                }
+
+                if (!Less(k, index))
+                {
+                    break;
+                }
+
+                Degis(index, k);
+                index = k;
+            }
+        }
+
+        private void Swim(int index)
+        {
+            int k;
+
+            while (index / 2 > 0)
+            {
+                k = index / 2;
+
+                if (!Less(index, k))
+                {
+                    break;
+                }
+
+                Degis(index, k);
+                index = k;
+            }
+        }
+
+        internal bool IsEmpty()
+        {
+            return mCount == 0;
+        }
+
+        internal void Enqueue(T item)
+        {
+            if (mCount == mArray.Length - 1)
+            {
+                Expand(mArray.Length * 3);
+            }
+
+            mArray[++mCount] = item;
+            Swim(mCount);
+        }
+
+        internal T Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                T item = mArray[1];
+                mArray[1] = mArray[mCount];
+                mArray[mCount--] = default(T);
+
+                Sink(1);
+
+                return item;
+            }
+
+            return default(T);
+        }
+
+        internal T Find(T item, out int index)
+        {
+            index = -1;
+            if (!IsEmpty())
+            {
+                int i = 0;
+
+                while (++i <= mCount)
+                {
+                    if (mArray[i].Equals(item))
+                    {
+                        index = i;
+                        return mArray[i];
+                    }
+                }
+            }
+
+            return default(T);
+        }
+
+        internal void Kaldir(int index)
+        {
+            if (index > 0 && index <= mCount)
+            {
+                mArray[index] = mArray[mCount];
+                mArray[mCount--] = default(T);
+                Sink(index);
+            }
+        }
+
+        #endregion Methods
+    }
+
+    internal sealed class LinearKaristir<T>
+    {
+
+        private Random mRandom;
+
+
+        internal LinearKaristir()
+        {
+            int seed = 37 + 37 * ((int)DateTime.Now.TimeOfDay.TotalSeconds % 37);
+            mRandom = new Random(seed);
+        }
+
+        internal void Shuffle(T[] array)
+        {
+            int position;
+            for (int i = 0; i < array.Length; i++)
+            {
+                position = Rastgele(0, i);
+                Swap(array, i, position);
+            }
+        }
+
+        private int Rastgele(int min, int max)
+        {
+            return mRandom.Next(min, max);
+        }
+
+        private void Swap(T[] a, int i, int j)
+        {
+            T temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+
+    }
+}
